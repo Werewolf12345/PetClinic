54b70c5bbedf3fe14d18fb657d2267f66ed70c81
Alexey Boriskin <alexei.boriskin@gmail.com>
2017-03-10 09:46:58 -0600

    Server has been changed to PestgreSQL

---
+++ petclinic_coffees.sql
@@ -0,0 +1,5 @@
+INSERT INTO petclinic.coffees (COF_NAME, SUP_ID, PRICE, SALES, TOTAL) VALUES ('Colombian', 101, 7.99, 50, 225);
+INSERT INTO petclinic.coffees (COF_NAME, SUP_ID, PRICE, SALES, TOTAL) VALUES ('French_Roast', 49, 8.99, 0, 0);
+INSERT INTO petclinic.coffees (COF_NAME, SUP_ID, PRICE, SALES, TOTAL) VALUES ('Espresso', 150, 9.99, 0, 0);
+INSERT INTO petclinic.coffees (COF_NAME, SUP_ID, PRICE, SALES, TOTAL) VALUES ('Colombian_Decaf', 101, 8.99, 0, 0);
+INSERT INTO petclinic.coffees (COF_NAME, SUP_ID, PRICE, SALES, TOTAL) VALUES ('French_Roast_Decaf', 49, 9.99, 0, 0);
